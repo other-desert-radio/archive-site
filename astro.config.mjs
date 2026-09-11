be04eb7d5abdbec7f9236c6f7c394dead1f0e6f3
garrepi <jacgarrett18@gmail.com>
2026-09-11 14:29:26 -0700

    add new linter (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [react()],
+	output: "static",
+	integrations: [react()],
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,10 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import astro from "eslint-plugin-astro";
 
 export default [
-  {
-    ignores: [".astro/**", "dist/**", "node_modules/**"],
-  },
-  eslint.configs.recommended,
-  ...astro.configs["flat/recommended"],
-  eslintConfigPrettier,
+	{
+		ignores: [".astro/**", "dist/**", "node_modules/**"],
+	},
+	eslint.configs.recommended,
+	...astro.configs["flat/recommended"],
+	eslintConfigPrettier,
 ];
