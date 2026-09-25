951e2c2ad0aee42d734a51d4c58d6b97256c56cc
John Garrett <jacgarrett18@gmail.com>
2026-09-25 12:03:03 -0700

    add scafolding for UI

--- src/components/shows/ShowCard.tsx
+++ src/components/shows/ShowCard.tsx
@@ -11,19 +11,30 @@ type Props = {
 
 export default function ShowCard({ show, base, tags }: Props) {
 	return (
-		<a
-			href={`${base}/shows/${show.id}`}
-			className={styles["show-card"]}
-			aria-label={`View ${show.title}`}
-		>
+		<article className={styles["show-card"]}>
 			<div className={styles["placeholder-widget"]} />
 			<div className={styles["trailing-content"]}>
-				<span className={styles["show-title"]}>{show.title}</span>
+				<a
+					href={`${base}/shows/${show.id}`}
+					className={styles["show-title"]}
+					aria-label={`View ${show.title}`}
+				>
+					{show.title}
+				</a>
 				<div className={styles["tag-date-container"]}>
 					<TagContainer tagIds={show.tagIds} tags={tags} />
 					<span className={styles["show-date"]}>{show.date}</span>
 				</div>
+				{show.djs.map((dj) => (
+					<a
+						key={dj.id}
+						href={`${base}/djs/${dj.id}`}
+						aria-label={`View ${dj.title}`}
+					>
+						{dj.title}
+					</a>
+				))}
 			</div>
-		</a>
+		</article>
 	);
 }
